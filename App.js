05e3fb791bafa2aedbaee5db44cc13ad1c18ae65
phucnt2002 <nguyenphuc2002ok@gmail.com>
2023-02-07 23:47:39 +0700

    delete add navigation

--- App.js
+++ App.js
@@ -4,13 +4,16 @@ import {Login, Main, Register, Settings} from './screens'
 import {FlatListSan} from './components'
 import { useState } from 'react';
 import GlobalStyles from './GlobalStyles';
+import UITag from './navigation/UITab';
+import AppNavigation from './navigation/AppNavigation'
 export default function App() {
 
   return (
     // <Login></Login>
     // <Settings></Settings>
     <SafeAreaView style={GlobalStyles.droidSafeArea}>
-      <FlatListSan></FlatListSan>
+      {/* <FlatListSan></FlatListSan> */}
+      <AppNavigation></AppNavigation>
     </SafeAreaView>
     // <Register></Register>
     // <View style={{flex: 1,
